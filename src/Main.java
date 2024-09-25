7185ac5d5d4cf1e54408aa1cfdfb1400f7def7c3
Ke1en <Andpey-dok@mail.ru>
2024-09-26 00:27:08 +0600

    Converter updated

--- src/Main.java
+++ src/Main.java
@@ -15,40 +15,49 @@ public class Main {
                 case "kg to pounds":
                     System.out.println("Input weight in kg");
                     userVarInput = scanner.nextDouble();
+
                     System.out.println(userVarInput + " kg in pounds: " + userVarInput*2.20462);
+                    scanner.nextLine();
                     break;
                 case "pounds to kg":
                     System.out.println("Input weight in pounds");
                     userVarInput = scanner.nextDouble();
                     System.out.println(userVarInput + " pounds in kg: " + userVarInput*0.453592);
+                    scanner.nextLine();
                     break;
                 case "rubles to dollars":
                     System.out.println("Input amount of rubles");
                     userVarInput = scanner.nextDouble();
                     System.out.println(userVarInput + " rubles in dollars is : " + userVarInput/100);
+                    scanner.nextLine();
                     break;
                 case "dollars to rubles":
                     System.out.println("Input amount of dollars");
                     userVarInput = scanner.nextDouble();
                     System.out.println(userVarInput + " dollars in rubles is : " + userVarInput*100);
+                    scanner.nextLine();
                     break;
                 case "F temperature to C":
                     System.out.println("Input temperature in F");
                     userVarInput = scanner.nextDouble();
                     System.out.println(userVarInput + " F in C is " + ((userVarInput-32) * 5 / 9));
+                    scanner.nextLine();
                     break;
                 case "C temperature to F":
                     System.out.println("Input temperature in C");
                     userVarInput = scanner.nextDouble();
                     System.out.println(userVarInput + " C in F is " + ((userVarInput * 1.8) + 32));
+                    scanner.nextLine();
                     break;
                 case "exit":
                     strToChoose = "exit";
                     break;
                 default:
                     System.out.println("Choose what to do again!");
+                    scanner.nextLine();
                     break;
             }
+            System.out.println("Choose what to do again!");
             strToChoose = scanner.nextLine();
         }
     }
